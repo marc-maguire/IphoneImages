ef46c3609dd9daf5f5ce1c761b06035585a660fc
Marc Maguire <maguire.marc@gmail.com>
2017-05-22 13:22:28 -0400

    Initial Commit

---
+++ Phone Images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Phone Images
+//
+//  Created by Marc Maguire on 2017-05-22.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Phone Images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phone Images
+//
+//  Created by Marc Maguire on 2017-05-22.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
